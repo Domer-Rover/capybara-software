287c70e98048c5812d44737b74e515e91fc5634e
bemndy <brandonprvbox@gmail.com>
2026-04-08 18:33:50 -0400

    joystick not working

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -59,9 +59,9 @@ while true; do
     LAUNCH_COUNT=$((LAUNCH_COUNT + 1))
     echo -e "\n${BOLD}--- Launch attempt #${LAUNCH_COUNT} ---${NC}"
 
-    ros2 launch capybara_bringup capybara.launch.xml \
-    use_joystick:=true use_mock_hardware:=false      \
-	launch_rviz:=false launch_zed:=false
+    ros2 launch capybara_bringup capybara_foxglove.launch.py \
+    use_joystick:=false use_mock_hardware:=false      \
+	launch_rviz:=false launch_zed:=true
     EXIT_CODE=$?
 
     if [ $EXIT_CODE -eq 0 ]; then
